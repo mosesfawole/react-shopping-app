c3a75d41b2989dcd07406e340031d733fa4a8876
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-29 21:56:10 +0100

    render navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import NavbarComponent from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-
   return (
     <div className="">
-     
+      <NavbarComponent></NavbarComponent>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
