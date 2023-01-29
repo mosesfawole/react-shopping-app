f516fb546e22190e556b236065a1575b79262332
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-29 22:00:45 +0100

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import NavbarComponent from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="">
       <Container>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
         <NavbarComponent></NavbarComponent>
       </Container>
     </div>
